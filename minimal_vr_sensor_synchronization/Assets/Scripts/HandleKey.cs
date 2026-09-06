e3d088a14dee45aeca10d6f36691b6cb05d65103
Felix Dollack <felix.dollack@googlemail.com>
2026-09-06 08:21:59 -0600

    Expose trigger key setting in editor and simplify call

--- minimal_vr_sensor_synchronization/Assets/Scripts/HandleKey.cs
+++ minimal_vr_sensor_synchronization/Assets/Scripts/HandleKey.cs
@@ -5,26 +5,24 @@ public class HandleKey : MonoBehaviour
 {
     [SerializeField] SerialClientWriteOnly serialClient;
 
+    [Header("Trigger Key")]
+    [SerializeField] Key triggerKey = Key.T;
+
     void Start()
     {
         // Omit this if "Open On Start" is enabled on SerialClient.
         serialClient.Open();
     }
 
-    public void Send()
-    {
-        if (serialClient.SendLine("1"))
-            Debug.Log("Sent serial line: 1");
-        else
-            Debug.LogWarning("Serial device is not connected.");
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current?.lKey.wasPressedThisFrame == true)
+        if (Keyboard.current?[triggerKey].wasPressedThisFrame == true)
         {
-            Send();
+            if (serialClient.SendLine("1"))
+                Debug.Log("Sent serial line: 1");
+            else
+                Debug.LogWarning("Serial device is not connected.");
         }
     }
 }
